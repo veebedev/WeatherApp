c1cf2f64723387f15bb28ffe60413721ac29b726
kjn373 <krzen31@gmail.com>
2025-05-09 02:52:24 +0000

    updated ui

--- src/main.ts
+++ src/main.ts
@@ -17,12 +17,12 @@ function error() {
 }
 
 
-async function getWeatherInfo(lat:number,lon:number){
-    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=ff5b29f4f9c8a46d01ef96c66b2425dd`
+async function getWeatherInfo(location:string, api_key:string){
+    const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metrics&appid=${api_key}`
     try {
         const response = await fetch(weatherURL);
         if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
+          throw new Error(`Response status: ${response.status}`);
         }
         const json = await response.json();
         console.log(json);
@@ -30,26 +30,6 @@ async function getWeatherInfo(lat:number,lon:number){
         const city = json.name ;
         alert(`It is ${temp} cewlcius in ${city}`)
     }catch (error:unknown) {
-      console.error(error.message);
+      console.error("an error occured");
     }
 }
-
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
